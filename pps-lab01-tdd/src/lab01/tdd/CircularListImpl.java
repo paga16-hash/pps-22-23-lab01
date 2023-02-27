d831cb32d4695086e3bd55bea16d6891853f7abf
Alberto Paganelli <albi1600@gmail.com>
2023-02-27 10:18:29 +0100

    impl add elements and empty test

---
+++ pps-lab01-tdd/src/lab01/tdd/CircularListImpl.java
@@ -0,0 +1,40 @@
+package lab01.tdd;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class CircularListImpl implements CircularList {
+
+    private final List<Integer> list = new ArrayList<>();
+
+    @Override
+    public void add(int element) {
+        this.list.add(element);
+    }
+
+    @Override
+    public int size() {
+        return this.list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.list.isEmpty();
+    }
+
+    @Override
+    public Optional<Integer> next() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Integer> previous() {
+        return Optional.empty();
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,17 +1,36 @@
+import lab01.tdd.CircularList;
+import lab01.tdd.CircularListImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
+    private final CircularList circularList = new CircularListImpl();
 
     @Disabled
-    @Test public void testTodo(){
+    @Test
+    public void testTodo(){
         Assertions.fail();
+    }
+
+    @Test
+    public void testElementsInsertion(){
+        this.circularList.add(1);
+        this.circularList.add(2);
+        this.circularList.add(3);
+        assertEquals(3, this.circularList.size());
+    }
+
+    @Test
+    public void testEmptyList(){
+        assertTrue(this.circularList.isEmpty());
     }
 
 }
