35613704c5fc09278d589f85ea96f627bc9e4d8b
Alberto Paganelli <albi1600@gmail.com>
2023-02-28 17:57:31 +0100

    finish test forwardIter es2.2

--- pps-lab01-tdd/src/lab01es02/tdd/CircularListImpl.java
+++ pps-lab01-tdd/src/lab01es02/tdd/CircularListImpl.java
@@ -1,4 +1,5 @@
 package lab01es02.tdd;
+import javax.management.InstanceNotFoundException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -6,7 +7,7 @@ import java.util.Optional;
 
 public class CircularListImpl implements CircularList {
 
-    private final List<Integer> list = new ArrayList<>();
+    private final lab01.tdd.CircularList list = new lab01.tdd.CircularListImpl();
     @Override
     public void add(int element) {
         this.list.add(element);
@@ -24,11 +25,32 @@ public class CircularListImpl implements CircularList {
 
     @Override
     public Iterator<Integer> forwardIterator() {
-        return null;
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                //TODO Gestione caso con lista vuota
+                return list.next().orElseThrow();
+            }
+        };
     }
 
     @Override
     public Iterator<Integer> backwardIterator() {
-        return null;
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return null;
+            }
+        };
     }
 }
