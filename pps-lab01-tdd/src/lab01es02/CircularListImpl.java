1fa034a84f2b84a3991b50a9f890c368f870a1e4
Alberto Paganelli <albi1600@gmail.com>
2023-02-28 15:53:32 +0100

    created new package, interface and test class

---
+++ pps-lab01-tdd/src/lab01es02/CircularListImpl.java
@@ -0,0 +1,30 @@
+package lab01es02;
+
+import java.util.Optional;
+
+public class CircularListImpl implements CircularList {
+    @Override
+    public void add(int element) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Optional<Integer> forwardIterator() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Integer> backwardIterator() {
+        return Optional.empty();
+    }
+}
